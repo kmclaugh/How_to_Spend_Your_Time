286ff3318834d2e0c35903ddf91980c61ffad726
Kevin McLaughlin <kevin@studentloangenius.com>
2018-04-13 12:29:56 -0700

    Updated to class

--- spend_time.js
+++ spend_time.js
@@ -1,75 +1,98 @@
-/**
- * A simple color interpolator.
- * Parses a `from` and `to` HSL string in the format
- * `hsl(200, 100%, 50%)` and a step value between 0 and 1
- * and returns a new HSL string.
- * An optional `precision` value may be provided for the
- * new HSL string
- *
- * @param {string} from
- * @param {string} to
- * @param {number} step
- * @param {precision} number
- */
+
+var regex = /^hsl\(\s*([\-|\d|\.]*)\s*,\s*([\d|\.]*)%\s*,\s*([\d|\.]*)%/;
+function interpolate (start, end, step, precision) {
+  precision = precision != null ? precision : 0;
+  start = start.match(regex);
+  end = end.match(regex);
+  var
+    startH = +start[1],
+    startS = +start[2],
+    startL = +start[3],
+    endH   = +end[1],
+    endS   = +end[2],
+    endL   = +end[3];
+
+  var
+    h = (startH - (startH - endH) * step).toFixed(precision),
+    s = (startS - (startS - endS) * step).toFixed(precision),
+    l = (startL - (startL - endL) * step).toFixed(precision);
+
+  return 'hsl(' + h + ', ' + s + '%, ' + l + '%)';
+}
+
+class Activity {
+  constructor(word, importance, urgency, experience, remember, active) {
+    this.word = word;
+    this.importance = importance;
+    this.urgency = urgency;
+    this.experience = experience;
+    this.remember = remember;
+    this.active = active;
+    this.saturation = 100
+    this.html = null;
+  }
+
+  setHTML(){
+    if (this.active){
+      this.html = $("#currentActivity").html();
+      activitiesList.prepend(currentActivity);
+    }
+  }
+
+  findColor(){
+    var red = "hsl(0, "+this.saturation+"%, 50%)"
+    var green = "hsl(120, "+this.saturation+"%, 25%)"
+    this.color = interpolate(red, green, this.experience/100);
+    return this.color;
+  }
+
+  findSize(){
+    this.fontSize = 32*(this.importance/100)+16
+    return this.fontSize;
+  }
+
+  findWordTransparency(){
+    this.wordTransparency = this.remember/100;
+    return this.wordTransparency;
+  }
+
+  findContainerTransparency(){
+    this.containerTransparency = this.urgency/100;
+    return this.containerTransparency;
+  }
+
+}
 
 $(document).ready(function() {
-  var regex = /^hsl\(\s*([\-|\d|\.]*)\s*,\s*([\d|\.]*)%\s*,\s*([\d|\.]*)%/;
-  function interpolate (start, end, step, precision) {
-    precision = precision != null ? precision : 0;
-    start = start.match(regex);
-    end = end.match(regex);
-    var
-      startH = +start[1],
-      startS = +start[2],
-      startL = +start[3],
-      endH   = +end[1],
-      endS   = +end[2],
-      endL   = +end[3];
+  
 
-    var
-      h = (startH - (startH - endH) * step).toFixed(precision),
-      s = (startS - (startS - endS) * step).toFixed(precision),
-      l = (startL - (startL - endL) * step).toFixed(precision);
-
-    return 'hsl(' + h + ', ' + s + '%, ' + l + '%)';
-  }
-
-  function findColor(experience, saturation){
-    var red = "hsl(0, "+saturation+"%, 50%)"
-    var green = "hsl(120, "+saturation+"%, 25%)"
-    var color = interpolate(red, green, experience/100);
-    return color;
-  }
-
-  function findSize(importance){
-    var fontSize = 32*(importance/100)+16
-    return fontSize;
-  }
-
-  function findTransparency(remember){
-    var transparency = remember/100;
-    return transparency;
-  }
-
+  var savedActivities = [];
   var importance = 50;
   var experience = 50;
   var remember = 50;
   var urgency = 50;
   var saturation = 100;
 
-  var color = findColor(experience, saturation);
-  var size = findSize(importance);
-  var wordTransparency = findTransparency(remember);
-  var containerTransparency = findTransparency(urgency);
+  let currentActivity = new Activity(
+    "Enter Activity",
+    50,
+    50,
+    50,
+    50,
+    100,
+    true,
+
+  );
 
   var currentWord = document.getElementsByClassName('word')[0];
+  var color = currentActivity.findColor()
   currentWord.style.fill = color;
   currentWord.style.stroke = color;
-  currentWord.setAttribute("font-size", size);
-  currentWord.setAttribute("fill-opacity", wordTransparency);
+  currentWord.setAttribute("font-size", currentActivity.findSize());
+  currentWord.setAttribute("fill-opacity", currentActivity.findWordTransparency());
   var wordContainer = document.getElementsByClassName('wordContainer')[0];
   wordContainer.style.fill = color;
-  wordContainer.setAttribute("fill-opacity", containerTransparency);
+  wordContainer.setAttribute("fill-opacity", currentActivity.findContainerTransparency());
 
   //importance slider
   var importanceSlider = document.getElementById("importanceRange");
@@ -78,35 +101,30 @@ $(document).ready(function() {
 
   importanceSlider.oninput = function() {
     importanceOutput.innerHTML = this.value;
-    importance = this.value;
-    size = findSize(importance);
-    currentWord.setAttribute("font-size", size);
+    currentActivity.importance = this.value;
+    currentWord.setAttribute("font-size", currentActivity.findSize());
   }
 
   //urgency slider
   var urgencySlider = document.getElementById("urgencyRange");
   var urgencyOutput = document.getElementById("urgencyValue");
   urgencyOutput.innerHTML = urgencySlider.value;
-
   urgencySlider.oninput = function() {
     urgencyOutput.innerHTML = this.value;
     var rawValue = this.value;
-
-    urgency = 50 - rawValue/2;
-    containerTransparency = findTransparency(urgency);
-    wordContainer.setAttribute("fill-opacity", containerTransparency);
+    currentActivity.urgency = 50 - rawValue/2;
+    wordContainer.setAttribute("fill-opacity", currentActivity.findContainerTransparency());
   }
 
   //experience slider
   var experienceSlider = document.getElementById("experienceRange");
   var experienceOutput = document.getElementById("experienceValue");
   experienceOutput.innerHTML = experienceSlider.value;
-
   experienceSlider.oninput = function() {
     experienceOutput.innerHTML = this.value;
     rawValue = this.value;
-    experience = Number(rawValue) + 50
-    color = findColor(experience, saturation);
+    currentActivity.experience = Number(rawValue) + 50
+    color = currentActivity.findColor();
     currentWord.style.fill = color;
     currentWord.style.stroke = color;
     wordContainer.style.fill = color;
@@ -119,23 +137,36 @@ $(document).ready(function() {
 
   rememberSlider.oninput = function() {
     rememberOutput.innerHTML = this.value;
-    remember = this.value;
-    wordTransparency = findTransparency(remember);
-    currentWord.setAttribute("fill-opacity", wordTransparency);
+    currentActivity.remember = this.value;
+    currentWord.setAttribute("fill-opacity", currentActivity.findWordTransparency());
   }
-
-
-
 
   //word input
   $('#currentActivityInput').on('input', function() {
-    $('#currentOutput').text($(this).val());
+    currentActivity.word = $(this).val();
+    $('#currentOutput').text(currentActivity.word);
+
   });
 
   var activitiesList = $("#activitiesList");
   $("#addActivity").on('click', function(){
-    currentActivity = $("#currentActivity").html();
-    activitiesList.prepend(currentActivity);
+    
+
+    //word, importance, urgency, experience, remember, active
+    let savedActivity = new Activity(
+      currentActivity.word,
+      currentActivity.importance,
+      currentActivity.urgency,
+      currentActivity.experience,
+      currentActivity.remember,
+      false
+    );
+    savedActivity.html = $("#currentActivity").html();
+
+    savedActivities.push(savedActivity);
+    activitiesList.prepend(savedActivity.html);
   })
+
+
 
 });
