2acd9622f6268d70acf975f6f6b2e9b96c49cb8f
Kevin McLaughlin <kevin@studentloangenius.com>
2018-04-20 09:17:30 -0500

    Finished unpolish version of itneractive

--- spend_time.js
+++ spend_time.js
@@ -21,8 +21,9 @@ function interpolate (start, end, step, precision) {
 }
 
 class Activity {
-  constructor(word, importance, urgency, experience, remember, active) {
+  constructor(word, importance, urgency, experience, remember, active, chronology) {
     this.word = word;
+    this.chronology = chronology;
     this.importance = importance;
     this.urgency = urgency;
     this.experience = experience;
@@ -42,7 +43,8 @@ class Activity {
   findColor(){
     var red = "hsl(0, "+this.saturation+"%, 50%)"
     var green = "hsl(120, "+this.saturation+"%, 25%)"
-    this.color = interpolate(red, green, this.experience/100);
+    var vizValue = this.experience + 50;
+    this.color = interpolate(red, green, vizValue/100);
     return this.color;
   }
 
@@ -56,31 +58,47 @@ class Activity {
     return this.wordTransparency;
   }
 
-  findContainerTransparency(){
-    this.containerTransparency = this.urgency/100;
-    return this.containerTransparency;
+  findContainerBorder(){
+    this.containerBorder = 2*this.urgency;
+    return this.containerBorder;
+  }
+
+  calculateEnjoymentScore(){
+    this.enjoyment = Math.round(this.remember/10 * this.experience);
+  }
+
+  calculateProductivityScore(){
+    this.productivity = Math.round(this.urgency/10 * this.importance);
+  }
+
+  calulcateFlourishingScore(){
+    this.calculateEnjoymentScore();
+    this.calculateProductivityScore();
+    this.flourishing= Math.round(this.productivity + this.enjoyment);
   }
 
 }
 
 $(document).ready(function() {
   
+  var currentSort = "chronology";
 
   var savedActivities = [];
   var importance = 50;
-  var experience = 50;
+  var experience = 0;
   var remember = 50;
-  var urgency = 50;
-  var saturation = 100;
+  var urgency = 2;
+  var counter = 0;
 
   let currentActivity = new Activity(
+    /*word, importance, urgency, experience, remember, active, counter*/
     "Enter Activity",
-    50,
-    50,
-    50,
-    50,
-    100,
+    importance,
+    urgency,
+    experience,
+    remember,
     true,
+    counter
 
   );
 
@@ -92,7 +110,16 @@ $(document).ready(function() {
   currentWord.setAttribute("fill-opacity", currentActivity.findWordTransparency());
   var wordContainer = document.getElementsByClassName('wordContainer')[0];
   wordContainer.style.fill = color;
-  wordContainer.setAttribute("fill-opacity", currentActivity.findContainerTransparency());
+  wordContainer.setAttribute("fill-opacity", currentActivity.findContainerBorder());
+
+  //Scores
+  currentActivity.calulcateFlourishingScore();
+  var productivityValue = document.getElementById("productivityValue");
+  productivityValue.innerHTML = currentActivity.productivity;
+  var enjoymentValue = document.getElementById("enjoymentValue");
+  enjoymentValue.innerHTML = currentActivity.enjoyment;
+  var flourishingValue = document.getElementById("flourishingValue");
+  flourishingValue.innerHTML = currentActivity.flourishing;
 
   //importance slider
   var importanceSlider = document.getElementById("importanceRange");
@@ -101,8 +128,11 @@ $(document).ready(function() {
 
   importanceSlider.oninput = function() {
     importanceOutput.innerHTML = this.value;
-    currentActivity.importance = this.value;
+    currentActivity.importance = Number(this.value);
     currentWord.setAttribute("font-size", currentActivity.findSize());
+    currentActivity.calulcateFlourishingScore();
+    productivityValue.innerHTML = currentActivity.productivity;
+    flourishingValue.innerHTML = currentActivity.flourishing;
   }
 
   //urgency slider
@@ -111,9 +141,11 @@ $(document).ready(function() {
   urgencyOutput.innerHTML = urgencySlider.value;
   urgencySlider.oninput = function() {
     urgencyOutput.innerHTML = this.value;
-    var rawValue = this.value;
-    currentActivity.urgency = 50 - rawValue/2;
-    wordContainer.setAttribute("fill-opacity", currentActivity.findContainerTransparency());
+    currentActivity.urgency = this.value;
+    wordContainer.setAttribute("style", "stroke-width:"+currentActivity.findContainerBorder());
+    currentActivity.calulcateFlourishingScore();
+    productivityValue.innerHTML = currentActivity.productivity;
+    flourishingValue.innerHTML = currentActivity.flourishing;
   }
 
   //experience slider
@@ -122,12 +154,14 @@ $(document).ready(function() {
   experienceOutput.innerHTML = experienceSlider.value;
   experienceSlider.oninput = function() {
     experienceOutput.innerHTML = this.value;
-    rawValue = this.value;
-    currentActivity.experience = Number(rawValue) + 50
+    currentActivity.experience = Number(this.value);
     color = currentActivity.findColor();
     currentWord.style.fill = color;
     currentWord.style.stroke = color;
     wordContainer.style.fill = color;
+    currentActivity.calulcateFlourishingScore();
+    enjoymentValue.innerHTML = currentActivity.enjoyment;
+    flourishingValue.innerHTML = currentActivity.flourishing;
   }
 
   //remember slider
@@ -139,6 +173,9 @@ $(document).ready(function() {
     rememberOutput.innerHTML = this.value;
     currentActivity.remember = this.value;
     currentWord.setAttribute("fill-opacity", currentActivity.findWordTransparency());
+    currentActivity.calulcateFlourishingScore();
+    enjoymentValue.innerHTML = currentActivity.enjoyment;
+    flourishingValue.innerHTML = currentActivity.flourishing;
   }
 
   //word input
@@ -148,10 +185,8 @@ $(document).ready(function() {
 
   });
 
-  var activitiesList = $("#activitiesList");
   $("#addActivity").on('click', function(){
     
-
     //word, importance, urgency, experience, remember, active
     let savedActivity = new Activity(
       currentActivity.word,
@@ -159,14 +194,30 @@ $(document).ready(function() {
       currentActivity.urgency,
       currentActivity.experience,
       currentActivity.remember,
-      false
+      false,
+      counter
     );
     savedActivity.html = $("#currentActivity").html();
 
     savedActivities.push(savedActivity);
-    activitiesList.prepend(savedActivity.html);
+    sortActivities(savedActivities, currentSort, activitiesList)
+
+    counter += 1;
   })
 
+  $("#sortOrder").change(function() {
+    currentSort = $(this).val();
+    sortActivities(savedActivities, currentSort, activitiesList)
+  });
+
+function sortActivities(savedActivities, currentSort, activitiesList){
+  /*Sorts the activites accord to the sort order given */
+  savedActivities = _.orderBy(savedActivities, [currentSort], ['desc']);
+  var newHTML = _.map(savedActivities, 'html').join("");
+  $("#activitiesList").html(newHTML)
+
+
+}
 
 
 });
